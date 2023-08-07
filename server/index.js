81abbf493c6f9c8c17cb8950be1f52ab1cb97183
Christian Gonzales <chgonzales@fullscale.io>
2023-08-07 17:47:39 +0800

    Configure MongoDB

--- server/index.js
+++ server/index.js
@@ -1,13 +1,18 @@
+const colors = require("colors");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const dotenv = require("dotenv");
 const schema = require("./schema/schema");
+const connectDb = require("./config/db");
 
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// Connect to database
+connectDb();
 
 app.use(
   "/graphql",
