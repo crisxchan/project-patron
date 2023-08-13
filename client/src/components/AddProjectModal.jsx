4effd9b098c7b26f7b0b776c4a589d0dce2d4d7d
Christian Gonzales <chrisgonzales.online@gmail.com>
2023-08-13 20:44:24 +0800

    Complete add project feature

--- client/src/components/AddProjectModal.jsx
+++ client/src/components/AddProjectModal.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { FaList } from "react-icons/fa";
 import { useMutation, useQuery } from "@apollo/client";
-// import { ADD_CLIENT } from "../mutations/clientMutations";
+import { ADD_PROJECT } from "../mutations/projectMutation";
 import { GET_PROJECTS } from "../queries/projectQueries";
 import { GET_CLIENTS } from "../queries/clientQueries";
 
@@ -13,6 +13,19 @@ export default function AddClientModal() {
 
   const { loading, error, data } = useQuery(GET_CLIENTS);
 
+  const [addProject] = useMutation(ADD_PROJECT, {
+    variables: { name, description, status, clientId },
+    update(cache, { data: { addProject } }) {
+      const { projects } = cache.readQuery({ query: GET_PROJECTS });
+      cache.writeQuery({
+        query: GET_PROJECTS,
+        data: {
+          projects: [...projects, addProject],
+        },
+      });
+    },
+  });
+
   const onSubmit = (e) => {
     e.preventDefault();
 
@@ -20,6 +33,7 @@ export default function AddClientModal() {
       return alert("Please fill in all fields");
     }
 
+    addProject(name, description, status, clientId);
     setName("");
     setDescription("");
     setStatus("new");
@@ -103,18 +117,21 @@ export default function AddClientModal() {
                       </select>
                     </div>
                     <div className="mb-3">
-                      <label htmlFor="client" className="form-label">
-                        Status
+                      <label htmlFor="clientId" className="form-label">
+                        Client
                       </label>
                       <select
-                        id="client"
+                        id="clientId"
                         className="form-select"
-                        value={status}
-                        onChange={(e) => setStatus(e.target.value)}
+                        value={clientId}
+                        onChange={(e) => setClientId(e.target.value)}
                       >
-                        <option value="new">Not Started</option>
-                        <option value="progress">In Progress</option>
-                        <option value="completed">Completed</option>
+                        <option value="">Select client</option>
+                        {data.clients.map((client) => (
+                          <option key={client.id} value={client.id}>
+                            {client.name}
+                          </option>
+                        ))}
                       </select>
                     </div>
 
