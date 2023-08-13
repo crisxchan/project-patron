6c9a51afbb7b73578eae5bc0d00069da2a384544
Christian Gonzales <chrisgonzales.online@gmail.com>
2023-08-13 21:06:42 +0800

    Complete delete project feature

--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -2,6 +2,7 @@ import { Link, useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_PROJECT } from "../queries/projectQueries";
 import ClientInfo from "../components/ClientInfo";
+import DeleteProjectButton from "../components/DeleteProjectButton";
 import Spinner from "../components/Spinner";
 
 export default function Project() {
@@ -25,6 +26,8 @@ export default function Project() {
           <h5 className="mt-3">Project Status</h5>
           <p className="lead">{data.project.status}</p>
           <ClientInfo client={data.project.client} />
+
+          <DeleteProjectButton projectId={data.project.id} />
         </div>
       )}
     </>
